95bcc2d3a28fb9ae7b5ee224788facf36b1c73aa
amerr <amerrnath.21@gmail.com>
2019-06-14 14:10:49 +0200

    Thumbnail

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class ApplicationRoute extends Route {
   model() {
     return {
-      objectList: ['car', 'thief', 'Myself', 'House'],
+      objectList: ['car', 'thief', 'user', 'house'],
       actionList: ['steal', 'hit/bump', 'prosecutes']
     };
   }
